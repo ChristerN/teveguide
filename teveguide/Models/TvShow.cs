979ed827af28f3b3127d5d28c72352008bfcd515
Annica A <anan0104@student.miun.se>
2017-10-30 07:37:39 +0100

    Skapat klasser för User och TvShow

---
+++ teveguide/Models/TvShow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace teveguide.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Starttime { get; set; }
+        public DateTime Endtime { get; set; }
+        public string Substance { get; set; }
+        public string Category { get; set; }
+        public string Channel { get; set; }
+    }
+
+}
