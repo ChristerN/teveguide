edd307355499c135d34be394c33e44971066b8f0
ChristerN <chni9401@student.miun.se>
2017-11-01 14:45:40 +0100

    uppdaterade RouteConfig så att rätt sida startar. Ändrade puffarna. Nu länkar till specifika id i programtabellen. Uppdaterade index (plockade bort lite länkar)

--- teveguide/App_Start/RouteConfig.cs
+++ teveguide/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace teveguide
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "tv_shows", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
