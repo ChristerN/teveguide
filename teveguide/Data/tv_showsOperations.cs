d051f932dc4133348c7ecfb06d11dc0d2099cff6
ChristerN <chni9401@student.miun.se>
2017-11-01 16:25:01 +0100

    la till mappen Data och filen tv_showOperations

---
+++ teveguide/Data/tv_showsOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using teveguide.Models.db;
+
+namespace teveguide.Data
+{
+    public class tv_showsOperations
+    {
+        //private TeveGuideEntities2 db = new TeveGuideEntities2();
+
+
+        //public tv_shows GetPersonById(int id)
+        //{
+        //    var result =db.FirstOrDefault(p => p.Id == 5);
+
+        //    return result;
+        //}
+
+    }
+}
