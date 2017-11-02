2cc6219cb331eabdbb26e28a5668c5969ccf035c
ChristerN <chni9401@student.miun.se>
2017-11-02 14:21:04 +0100

    Lagt in att puffar styrs av vilket id som angivits i foreach-loop. Lagt till så att "Just nu visas" visar program som går just nu. (Lagt till if-sats i Index)

--- teveguide/Data/tv_showsOperations.cs
+++ teveguide/Data/tv_showsOperations.cs
@@ -10,15 +10,5 @@ namespace teveguide.Data
 {
     public class tv_showsOperations
     {
-        //private TeveGuideEntities2 db = new TeveGuideEntities2();
-        //protected void Page_Load(object sender, EventArgs e)
-        //{
-
-        //    using (TeveGuideEntities2 Data = new TeveGuideEntities2())
-        //    {
-        //        tv_shows Puff1 = Data.tv_shows.Single(tvs => tvs.Id == 12);
-        //        LabelReport.Text = Puff1.Title;
-        //    }
-        //}
     }
 }
