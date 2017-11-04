fc038018d5664291398416e1a4e9785b45c2b9de
annicaandersson <anan0104@student.miun.se>
2017-11-04 12:26:16 +0100

    Länkstruktur T -tablåer

--- teveguide/Controllers/HomeController.cs
+++ teveguide/Controllers/HomeController.cs
@@ -8,21 +8,49 @@ namespace teveguide.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Today()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Tomorrow()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult TwoDaysFromNow()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ThreeDaysFromNow()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult FourDaysFromNow()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult FiveDaysFromNow()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult SixDaysFromNow()
+        {
+            ViewBag.Message = "";
 
             return View();
         }
